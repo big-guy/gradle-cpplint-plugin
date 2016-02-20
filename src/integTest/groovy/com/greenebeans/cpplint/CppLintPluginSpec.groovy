2bd813fc087c4a8f1819253934efa5cd43674d2d
Sterling Greene <sterling.greene@gmail.com>
2016-02-20 16:12:27 -0500

    smoke tests

--- src/integTest/groovy/com/greenebeans/cpplint/CppLintPluginSpec.groovy
+++ src/integTest/groovy/com/greenebeans/cpplint/CppLintPluginSpec.groovy
@@ -1,15 +1,27 @@
 package com.greenebeans.cpplint
 
 class CppLintPluginSpec extends AbstractIntegrationSpec {
-    def setup() {
+
+    def "does nothing when CPP plugin is not applied"() {
+        given:
+        buildFile << """
+            apply plugin: 'com.greenebeans.cpplint'
+        """
+
+        expect:
+        def result = build("tasks", "--all")
+        !result.output.contains("installCppLint - Installs cpplint.py")
+    }
+
+    def "adds task when CPP plugin is applied"() {
+        given:
         buildFile << """
             apply plugin: 'org.gradle.cpp'
             apply plugin: 'com.greenebeans.cpplint'
-"""
-    }
+        """
 
-    def "does not explode"() {
         expect:
-        build("tasks")
+        def result = build("tasks", "--all")
+        result.output.contains("installCppLint - Installs cpplint.py")
     }
 }

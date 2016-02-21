64420290f55e8872dfc2171e2b15ed032cbc6f1c
Sterling Greene <sterling.greene@gmail.com>
2016-02-21 01:49:36 -0500

    make cpplint task incremental

--- src/main/groovy/com/greenebeans/cpplint/tasks/RunCppLint.groovy
+++ src/main/groovy/com/greenebeans/cpplint/tasks/RunCppLint.groovy
@@ -15,12 +15,17 @@
  */
 
 package com.greenebeans.cpplint.tasks
+
+import com.greenebeans.cpplint.tasks.internal.CppLintSpec
 import com.greenebeans.cpplint.tasks.internal.CppLintTool
 import com.greenebeans.cpplint.tasks.internal.DefaultCppLintSpec
+import org.gradle.api.Action
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.incremental.IncrementalTaskInputs
+import org.gradle.api.tasks.incremental.InputFileDetails
 import org.gradle.internal.operations.BuildOperationProcessor
 import org.gradle.internal.operations.logging.BuildOperationLogger
 import org.gradle.internal.operations.logging.BuildOperationLoggerFactory
@@ -29,6 +34,10 @@ import org.gradle.process.internal.ExecActionFactory
 
 import javax.inject.Inject
 
+/**
+ * TODO docs
+ * TODO consider CFG file
+ */
 class RunCppLint extends SourceTask {
     NativeBinarySpec nativeBinarySpec
 
@@ -38,11 +47,11 @@ class RunCppLint extends SourceTask {
     @Input
     String executablePath
 
-    @OutputDirectory
-    File reportFile = new File(project.buildDir, "reports/cpplint")
-
     @Input
     int verbosity = 0
+
+    @OutputDirectory
+    File reportFile = new File(project.buildDir, "reports/cpplint")
 
     @Inject
     protected BuildOperationLoggerFactory getBuildOperationLoggerFactory() {
@@ -60,18 +69,31 @@ class RunCppLint extends SourceTask {
     }
 
     @TaskAction
-    void run() {
+    void run(IncrementalTaskInputs taskInputs) {
         BuildOperationLogger buildOperationLogger = buildOperationLoggerFactory.newOperationLogger(getName(), reportFile)
         try {
             buildOperationLogger.start()
+
             CppLintTool tool = new CppLintTool(buildOperationProcessor, execActionFactory)
-            setDidWork(tool.execute(createSpec(buildOperationLogger)).didWork)
+            Collection<File> files = []
+            if (taskInputs.incremental) {
+                taskInputs.outOfDate(new Action<InputFileDetails>() {
+                    @Override
+                    void execute(InputFileDetails inputFileDetails) {
+                        files.add(inputFileDetails.file)
+                    }
+                })
+            } else {
+                files.addAll(getSource().getFiles())
+            }
+            CppLintSpec spec = createSpec(files, buildOperationLogger)
+            setDidWork(tool.execute(spec).didWork)
         } finally {
             buildOperationLogger.done()
         }
     }
 
-    private DefaultCppLintSpec createSpec(BuildOperationLogger buildOperationLogger) {
-        new DefaultCppLintSpec(counting, executablePath, verbosity, getSource().getFiles(), buildOperationLogger)
+    private CppLintSpec createSpec(Collection<File> files, BuildOperationLogger buildOperationLogger) {
+        return new DefaultCppLintSpec(counting, executablePath, verbosity, files, buildOperationLogger)
     }
 }
